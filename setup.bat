4c40568a16154a5ea53d6804da6717e0d5d6dae6
Naceur Ben Ammar <benammarn05@gmail.com>
2025-05-07 16:33:21 +0200

    update setup bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,20 @@
+@echo off
 
+rem echo installing GNU Make...
+rem winget install -e --id GnuWin32.Make
+rem echo Installing CMake
+rem winget install -e --id Kitware.CMake
 
-ms-vscode.cpptools-extension-pack
-platformio.platformio-ide
+echo Installing VS Code Extensions...
+
+rem Install ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-extension-pack
+
+rem Install platformio.platformio-ide
+code --install-extension platformio.platformio-ide
+
+rem Install tal7aouy.icons
+code --install-extension tal7aouy.icons
+
+echo Installation Complete.
+pause
